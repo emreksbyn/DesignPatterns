2ebe2f8c01d491d0c439387ea48615d21b78d299
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-06-30 13:53:28 +0300

    Composite and Proxy DP added.

---
+++ Composite/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+    Nesneler arasi hiyerarsi ve bu hiyerarsik nesnelere istedigim zaman ulasabilmek demektir.
+ */
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emre = new Employee { Name = "Emre" };
+
+            Employee ahmet = new Employee { Name = "Ahmet" };
+            emre.AddSubordinate(ahmet);
+
+            Employee mehmet = new Employee { Name = "Mehmet" };
+            emre.AddSubordinate(mehmet);
+
+            Employee can = new Employee { Name = "Can" };
+            ahmet.AddSubordinate(can);
+
+            Contractor cem = new Contractor { Name = "Cem" };
+            mehmet.AddSubordinate(cem);
+
+            Console.WriteLine(emre.Name);
+            foreach (Employee manager in emre)
+            {
+                Console.WriteLine("  " + manager.Name);
+                foreach (IPerson employee in manager)
+                {
+                    Console.WriteLine("    " + employee.Name);
+                }
+            }
+        }
+    }
+
+    interface IPerson
+    {
+        string Name { get; set; }
+    }
+
+    class Contractor : IPerson
+    {
+        public string Name { get; set; }
+    }
+
+    class Employee : IPerson, IEnumerable<IPerson>
+    {
+        List<IPerson> _subordinates = new List<IPerson>();
+
+        public void AddSubordinate(IPerson person)
+        {
+            _subordinates.Add(person);
+        }
+
+        public void RemoveSubordinate(IPerson person)
+        {
+            _subordinates.Remove(person);
+        }
+
+        public IPerson GetSubordinate(int index)
+        {
+            return _subordinates[index];
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerator<IPerson> GetEnumerator()
+        {
+            foreach (var subordinate in _subordinates)
+            {
+                // Enumerable yani itare edilebilir donmesi icin yield keywordu kullanilir.
+                yield return subordinate;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+
+/*
+    Bu DP' yi cache' leme sistemine benzetebiliriz. Bir sinif ve bu sinifin cagirdigi bir islem var. Ilk kez cagirdiginde mecburen o islem yapilacak. Fakat 2. kez cagirildiginda daha oncekini kullanma 
+ */
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditManager manager = new CreditManager();
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
+
+            CreditManagerProxy creditManagerProxy = new CreditManagerProxy();
+            Console.WriteLine(creditManagerProxy.Calculate());
+            Console.WriteLine(creditManagerProxy.Calculate());
+        }
+    }
+
+    abstract class CreditBase
+    {
+        public abstract int Calculate();
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+            return result;
+        }
+    }
+
+    class CreditManagerProxy : CreditBase
+    {
+        private CreditManager _creditManager;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManager == null)
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+            return _cachedValue;
+        }
+    }
+}
