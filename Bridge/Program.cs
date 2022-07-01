975a14d1665ce6e4a89122a6a77c41a17a9b9c9d
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-07-01 09:39:11 +0300

    Bridge and Decorator DP added

---
+++ Bridge/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/*
+    Bir nesnenin icinde soyutlanabilir, degistirilebilir kisimlar varsa onlari soyutlama teknikleri ile gerceklestirip kullanmaktir.
+ */
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.MessageSenderBase = new EmailSender();
+            customerManager.UpdateCustomer();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message saved!");
+        }
+
+        public abstract void Send(Body body);
+    }
+    class Body
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SmsSender", body.Title);
+        }
+    }
+
+    class EmailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EmailSender", body.Title);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body { Title = "Customer" });
+            Console.WriteLine("Customer Updated.");
+        }
+    }
+}
