369890920b8a14b2c0e269fb9503330313e69441
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-07-20 09:45:22 +0300

    Strategy and Observer DP added.

---
+++ Strategy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2022CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.CreditCalculatorBase = new Before2022CreditCalculator();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2022CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before2022");
+        }
+    }
+    class After2022CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after2022");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer manager business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
