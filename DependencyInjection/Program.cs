4a2ec5d71718051008cdab191f48fa7e6ec923c6
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-07-22 11:45:26 +0300

    Dependency Injection DP added.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,56 @@
+﻿using Ninject;
+using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+            //kernel.Bind<IProductDal>().To<NhProductDal>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+    class ProductManager
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            this._productDal = productDal;
+        }
+
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
